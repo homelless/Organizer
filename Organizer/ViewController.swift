3800544ed0c4155b333d140938be685e397b868f
vladislav <107300120+homelless@users.noreply.github.com>
2025-06-19 15:49:06 +0300

    Initial Commit

---
+++ Organizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Organizer
+//
+//  Created by MacBookAir on 19.06.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

901cc4a6d5440b274fd4447131233f3a49bdf525
vladislav <107300120+homelless@users.noreply.github.com>
2025-06-19 15:58:17 +0300

    создание прототипа модели

--- Organizer/Models/Task.swift
+++ Organizer/Models/Task.swift
@@ -4,5 +4,27 @@
 //
 //  Created by MacBookAir on 19.06.25.
 //
-
+import UIKit
 import Foundation
+
+
+struct Task {
+    var id: UUID = UUID()
+    var title: String
+    var isComleted: Bool = false
+    var priority: Priority = .medium
+    
+    enum Priority: String, CaseIterable {
+        case low = "Низкий"
+        case medium = "Средний"
+        case high = "Высокий"
+        
+        var color: UIColor {
+            switch self {
+            case .low: return .systemGreen
+            case .medium: return .systemOrange
+            case .high: return .systemRed
+            }
+        }
+    }
+}
